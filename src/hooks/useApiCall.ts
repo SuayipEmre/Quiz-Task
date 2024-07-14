78bf32d0f80e9f4d92eb0f76b04645ce99b85f01
Şuayip Emre <suayipemredev@gmail.com>
2024-07-15 02:34:09 +0300

    feat: add custom API hook for handling requests

---
+++ src/hooks/useApiCall.ts
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const BASE_URL = 'https://jsonplaceholder.typicode.com';
+
+export const useApiCall  = () => {
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+
+    const apiCall = async (path : string) => {
+        setLoading(true);
+        setError(false);
+
+        try {
+            const { data } = await axios.get(`${BASE_URL}/${path}`);
+            setLoading(false);
+            return data;
+        } catch (err) {
+            setError(true);
+            setLoading(false);
+            return null;
+        }
+    };
+
+    return { apiCall, loading, error };
+};
